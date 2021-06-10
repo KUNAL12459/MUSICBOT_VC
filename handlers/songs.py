903bd37bf6eda21797900c2697e7558144b68b6a
Saif Ali <83064362+saifalisew1508-op@users.noreply.github.com>
2021-06-10 10:09:06 +0530

    Update songs.py

--- handlers/songs.py
+++ handlers/songs.py
@@ -45,13 +45,13 @@ def song(client, message):
         )
         print(str(e))
         return
-    m.edit("Downloading the song by ANSHIKA SINGH...")
+    m.edit("Downloading the song by 𝚅𝙴𝙽𝙾𝙼 𝚂𝙷𝙸𝚅𝙰𝙼...")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**🎵 Uploaded by [⚡ ANSHIKA SINGH ⚡](https://t.me/Anshikasingh_4).**'
+        rep = '**🎵 Uploaded by [⚡ 𝚅𝙴𝙽𝙾𝙼 𝚂𝙷𝙸𝚅𝙰𝙼 ⚡](https://t.me/SHIVAM9412).**'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
